5b4f3994edc660ba3491565e7beb0dbdfb4c2bf9
AkhilaK25 <102508563+AkhilaK25@users.noreply.github.com>
2022-07-06 19:13:03 +0530

    Update q2.2.py

--- codes/q2.2.py
+++ codes/q2.2.py
@@ -1,37 +1,23 @@
-#Importing numpy, scipy, mpmath and pyplot
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 
-#if using termux
-#import subprocess
-#import shlex
-#end if
-
-
-
-x = np.linspace(-4,4,30)#points on the x axis
+x = np.linspace(-5,5,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
-#randvar = np.random.normal(0,1,simlen)
 randvar = np.loadtxt('gau.dat',dtype='double')
-#randvar = np.loadtxt('gau.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
 
 	
-plt.plot(x.T,err) #plotting the CDF
+	
+plt.plot(x,err,'o') #plotting stimulation CDF
+plt.plot(x.T,err,color="orange") #plotting theory CDF
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
-#if using termux
-#plt.savefig('uni_cdf.pdf')
-#plt.savefig('uni_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
-#if using termux
-#plt.savefig('../figs/gauss_cdf.pdf')
-#plt.savefig('../figs/gauss_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/gauss_cdf.pdf"))
-#else
+plt.legend(["Numerical", "Theory"])
+plt.savefig('q2.2.png')
 plt.show() #opening the plot window
