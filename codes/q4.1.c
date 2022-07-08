e9d7d7b7054f4068152ca21e8991ecf026c49f73
AkhilaK25 <102508563+AkhilaK25@users.noreply.github.com>
2022-07-08 12:03:11 +0530

    Add files via upload

---
+++ codes/q4.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main() //main function begins
+{
+
+trian("tri.dat", 1000000);
+
+return 0;
+}
---
+++ codes/q4.2.py
@@ -0,0 +1,23 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-4,4,40) #points on the x axis
+simlen = int(1e6) #number of samplesgoogle drive
+err = [] #declaring probability list
+
+randvar = np.loadtxt('tri.dat',dtype='double')
+for i in range(0,40):
+	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+	err_n = np.size(err_ind) #computing the probability
+	err.append(err_n/simlen) #storing the probability values in a list
+
+	
+plt.plot(x,err,'o') #plotting stimulation CDF
+plt.plot(x.T,err,color="orange") #plotting theory CDF
+plt.grid() #creating the grid
+plt.xlabel('$x$')
+plt.ylabel('$F_X(x)$')
+plt.legend(["Numerical", "Theory"])
+plt.savefig('q4.1.png')
+plt.show() #opening the plot window
